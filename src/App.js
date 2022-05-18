10f3970f20d7fa713e8384695c8c2a83400338ce
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-19 01:03:17 +0600

    google signin implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './page/Footer';
 import Header from './page/Header';
 import Home from './page/Home';
+import SignIn from './page/SignIn';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/signIn' element={<SignIn></SignIn>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/page/SignIn.js
+++ src/page/SignIn.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import auth from '.././firebase.init';
 
 const SignIn = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    let from = location.state?.from?.pathname || "/";
+    
+    let errorElement;
+
+    if(loading){
+        return <p>Loading....</p>
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
     return (
-        <div>
-            <h2 className='text-center'>Sign in with google</h2>
+        <div className='container'>
+            <button
+                onClick={() => signInWithGoogle()}
+                className='btn btn-info w-50 d-block mx-auto my-2'>
+                <span className='px-2'>Google Sign In</span>
+            </button>
+            {errorElement}
         </div>
     );
 };
